71f17da4042e0f3a0938aec6a34cb51aaba80046
Oscar Gil <hello@oscargilbalaguer.com>
2025-03-21 18:31:30 +0100

    Repeat loop vs recursion

--- R/metrics.R
+++ R/metrics.R
@@ -1,11 +1,26 @@
 # Prompt the user to enter data
 
 get_metrics <- function(prompt) {
+  repeat {
     data <- suppressWarnings(as.numeric(readline(prompt)))
-    if (!is.na(data) && (data >= 0)) {
-        return(data)
+    if (!is.na(data) && data >= 0) {
+      return(data) # Única forma de salir del bucle repeat
+    } else {
+      cat("Invalid input. Please enter a non-negative number.\n")
+    }
+  }
+}
+
+# Esta es con recursión, pero no es recomendable (pregúntale a Perplexity)
+get_metrics <- function(prompt) {
+    data <- suppressWarnings(as.numeric(readline(prompt)))
+    if (!is.na(data) && data >= 0) {
+        return(data) # Única forma de salir del bucle repeat
     } else {
         cat("Invalid input. Please enter a non-negative number.\n")
+        repeat {
+            get_metrics(prompt)
+        }
     }
 }
 
