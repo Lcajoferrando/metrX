7b76a6e81519a38e638af9574de1e702ba632d15
Oscar Gil <hello@oscargilbalaguer.com>
2025-03-21 17:49:46 +0100

    first @get_metrics version

--- R/metrics.R
+++ R/metrics.R
@@ -1,8 +1,16 @@
 # Prompt the user to enter data
 
 get_metrics <- function(prompt) {
-    data <- as.integer(readline(prompt))
-    return(data)
+    data <- suppressWarnings(as.numeric(readline(prompt)))
+    if (!is.na(data)) {
+        if (data >= 0) {
+            return(data)
+        } else {
+            cat("Invalid input. Please enter a non-negative number.\n")
+        }
+    } else {
+        cat("Invalid input. Please enter a number.\n")
+    }
 }
 
 customer_retention <- get_metrics("Customer Retention: ")
