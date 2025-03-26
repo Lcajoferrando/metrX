b05f85d2d9fad37a40ff54cd41452d3d3d91e883
Oscar Gil <hello@oscargilbalaguer.com>
2025-03-26 23:13:24 +0100

    fix: function refactoring

--- R/metrics.R
+++ R/metrics.R
@@ -10,12 +10,22 @@ get_metrics <- function(prompt) {
   }
 }
 
-# Prompt the user to enter Customer Retention Rate metrics
-crr_prompts <- c(
+calculate_customer_retention <- function() {
+  prompts <- c(
                  "Customers at start of the period: ",
                  "Customers at the end of the period: ",
                  "New customers acquired during the period: ")
-customer_retention_rate <- sapply(crr_prompts, get_metrics)
+  customer_retention_rate_data <- sapply(prompts, get_metrics)
+
+  customers_at_start <- customer_retention_rate_data[1]
+  customers_at_end <- customer_retention_rate_data[2]
+  new_customers <- customer_retention_rate_data[3]
+
+  c_retention_rate <- ((customers_at_end - new_customers) /
+                           customers_at_start) * 100
+  
+  return (c_retention_rate)
+}
 
 # Prompt the user to enter Customer Churn Rate metrics
 ccr_prompts <- c(
@@ -42,51 +52,25 @@ nps_prompts <- c(
                  "Number of passives: ")
 net_promoter_score <- sapply(nps_prompts, get_metrics)
 
-# Extract the values from the vectors
-## Customer Retention Rate
-customer_retention_rate[[1]]
-customer_retention_rate[[2]]
-customer_retention_rate[[3]]
-
-## Customer Churn Rate
-customer_churn_rate[[1]]
-customer_churn_rate[[2]]
-
-## Customer Conversion Rate
-customer_conversion_rate[[1]]
-customer_conversion_rate[[2]]
-
-## Customer Satisfaction Score
-customer_satisfaction_score[[1]]
-customer_satisfaction_score[[2]]
-
-## Net Promoter Score
-net_promoter_score[[1]]
-net_promoter_score[[2]]
-net_promoter_score[[3]]
-
 # Calculate the metrics
 # Customer Retention Rate
-c_retention_result <- ((customer_retention_rate[[2]] -
-                          customer_retention_rate[[3]]) /
-                         customer_retention_rate[[1]]) * 100
-cat("Customer Retention Rate: ", c_retention_result, "%\n")
+cat("Customer Retention Rate: ", calculate_customer_retention(), "%\n")
 
 # Customer Churn Rate
-c_churn_rate <- (customer_churn_rate[[1]] /
-                   customer_churn_rate[[2]]) * 100
+c_churn_rate <- (customer_churn_rate[1] /
+                   customer_churn_rate[2]) * 100
 cat("Customer Churn Rate: ", c_churn_rate, "%\n")
 
 # Customer Conversion Rate
-c_conversion_rate <- (customer_conversion_rate[[1]] /
-                        customer_conversion_rate[[2]]) * 100
+c_conversion_rate <- (customer_conversion_rate[1] /
+                        customer_conversion_rate[2]) * 100
 cat("Customer Conversion Rate: ", c_conversion_rate, "%\n")
 
 # Customer Satisfaction Score
-c_satisfaction_score <- (customer_satisfaction_score[[1]] /
-                           customer_satisfaction_score[[2]]) * 100
+c_satisfaction_score <- (customer_satisfaction_score[1] /
+                           customer_satisfaction_score[2]) * 100
 cat("Customer Satisfaction Score: ", c_satisfaction_score, "%\n")
 
 # Net Promoter Score
-n_promoter_score <- (net_promoter_score[[1]] - net_promoter_score[[2]])
+n_promoter_score <- (net_promoter_score[1] - net_promoter_score[2])
 cat("Net Promoter Score: ", n_promoter_score, "%\n")
